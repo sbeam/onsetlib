821c01d35023e7aab513c4b7bb53b00fc9e60fdc
sbeam <sbeam@onsetcorps.net>
2008-11-05 16:48:48 +0000

    upgrade tinymce and add mcfilemanager plugin, and create fabulous browse/medialib filepicker for vzx

--- formex_js/rte-tinymce.js
+++ formex_js/rte-tinymce.js
@@ -9,17 +9,24 @@ tinyMCE.init({
     theme : "advanced",
     editor_selector : "mceAdvanced",
     apply_source_formatting : true,
-    plugins : "inlinepopups,advlink",
+    // plugins : "inlinepopups,advlink",
+
+    plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,filemanager",
+     
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
     theme_advanced_resizing : true,
     theme_advanced_resize_horizontal : false,
-    theme_advanced_buttons1 : "formatselect,separator,bold,italic,underline,separator,justifyleft,justifycenter,justifyright,separator,bullist,numlist,separator,outdent,indent,separator,forecolor,backcolor",
-    theme_advanced_buttons2 : "link,unlink,anchor,separator,image,charmap,hr,separator,visualaid,removeformat,cleanup,code,separator,undo,redo",
-    theme_advanced_buttons3 : "",
+    theme_advanced_buttons1 : "formatselect,|,bold,italic,underline,|,justifyleft,justifycenter,justifyright,|,bullist,numlist,|,outdent,indent,blockquote,|,forecolor,backcolor",
+    theme_advanced_buttons2 : "link,unlink,anchor,|,charmap,hr,|,visualaid,removeformat,cleanup,code,|,undo,redo,|,image,media",
+
+    theme_advanced_buttons3 : "tablecontrols,|,fullscreen",
+
     convert_fonts_to_spans : true,
     relative_urls : false,
     remove_script_host : true,
     convert_urls : false
 });
+
+
