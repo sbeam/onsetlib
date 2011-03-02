ad1132188f55e2007bace5c22c830aa150389e1e
Sam Beam <sbeam@onsetcorps.net>
2011-03-01 22:27:02 -0500

    cleanup crusty stuff; cleanup comments and make licenses consistent (MIT)

--- formex_field.class.php
+++ formex_field.class.php
@@ -1,33 +1,39 @@
 <?php
-//  formex_fields - PHP class to create form field object for formex
-//  Copyright (c) 2000-2004 S.Z.Beam, Onset Corps - sbeam@onsetcorps.net
+/*  formex_field - form field objects for use within formex
+ *
+ * Copyright 2000-2005 SBeam, Onset Corps - sbeam@onsetcorps.net
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-// +----------------------------------------------------------------------+
-//  This file is part of formex.
-//
-//  formex is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-// 
-//  formex is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-// 
-//  You should have received a copy of the GNU General Public License
-//  along with formex; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-// +----------------------------------------------------------------------+
-
-//!! Class formex_field()
-//!  class to create form field object for formex
-/*!
-    This class provides field objects that represent individual form
-    fields within formex. See private function documentation for details on
-    each field type - there is one function for each available field type
-    within this class.
-    */
+/**
+  * formex_field
+  *
+  * This class provides field objects that represent individual form
+  * fields within formex. See private function documentation for details on
+  * each field type - there is one function for each available field type
+  * within this class.
+  *
+  * This class is never called directly, only instantiated by formex->add_element()
+  * for usage of formex, see formex.class.php
+  *
+  */
 class formex_field 
 {
 
