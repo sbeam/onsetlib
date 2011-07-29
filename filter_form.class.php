fb0ddcd0546ecdeaf601aaa95f473b622696a134
Sam Beam <sbeam@onsetcorps.net>
2011-07-29 16:48:55 -0400

    some crusty html revisions

--- filter_form.class.php
+++ filter_form.class.php
@@ -4,10 +4,10 @@ require_once("formex.class.php");
     class filter_form extends formex
     {
         function table_start() {
-            return "<table cellpadding=\"3\" cellspacing=\"0\" border=\"0\">\n";
+            return "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n";
         }
         function table_row_begin($colspan=0) {
-            return sprintf("<td style=\"%s\" valign=\"middle\">", 
+            return sprintf("<td style=\"%s\">", 
                             $this->left_td_style);
         }
         function table_middle() {
